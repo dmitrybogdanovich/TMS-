e3e2c2fa5dcda6cc2e68c0d0d7bf478cd472cb8f
Dmitry Bogdanovich <d.bogdanovich@icloud.com>
2022-08-10 21:49:49 +0300

    no message

---
+++ MyFirstApp/MyFirstApp/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  MyFirstApp
+//
+//  Created by Дмитрий Богданович on 4.08.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    print ("hello world Dima!")
+        
+    }
+
+    
+    
+}
+
---
+++ MySecondProject/MySecondProject/MySecondProject/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  MySecondProject
+//
+//  Created by Дмитрий Богданович on 4.08.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print ("Hello world Dima!")
+    }
+
+    @IBAction func MyButton(_ sender: Any) {
+    }
+
+    
+}
+
